0497e552496aea1ddbb6f37c9e98ff000032a2cf
myportfolio-tech <eduardo@myportfolio.tech>
2021-02-20 12:02:04 -0500

    path to .env file has been configured

--- src/server/server.js
+++ src/server/server.js
@@ -5,12 +5,11 @@ const app = express();
 const axios = require('axios');
 const cors = require('cors');
 
-const dotenv = require('dotenv');
-dotenv.config(({ path: "../../" }));
-
-
+const dotenv = require('dotenv').config({path:__dirname+'/./../../.env'});
 const port = process.env.PORT;
-console.log(process.env.API_KEY);
+
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +18,8 @@ app.use(cors())
 
 app.use(express.static('src/dist'));
 app.use(favicon(path.join(__dirname, '../client/views', 'favicon.ico')));
+
+
 
 app.post('/api', async (req, res) => {
 

4884d450e7e5f1b821fb57c82b2e456f74b40fb3
myportfolio-tech <eduardo@myportfolio.tech>
2021-02-23 15:35:23 -0500

    setup for functions completed

--- __test__/handleCLick.test.js
+++ __test__/handleCLick.test.js
@@ -1,14 +1,9 @@
-const {handleSubmit} = require('../src/client/js/formHandler');
-// import {handleClick} from '../src/client/js/handleClick';
-
-// import {checkURL} from  '../src/client/js/checkURL';
-// import {cleanData} from '../src/client/js/cleanData';
-
+const {handleClick} = require('../src/client/js/handleClick');
 
 
 describe("Testing the submit functionality", () => {
     test("Testing the handleSubmit() function", () => {
-        expect(handleSubmit).toBeDefined();
+        expect(handleClick).toBeDefined();
     })
 })
 
--- src/client/js/formHandler.js
+++
@@ -1,9 +0,0 @@
-function handleSubmit(event) {
-    event.preventDefault();
-    
-    let formText = document.getElementById("name").value;
-    Client.checkForName(formText);
-    console.log("::: Form Submitted :::")
-}
-
-export {handleSubmit}
